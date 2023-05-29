389eec69912499cf7d5fad7fbfd58428dc82d5ea
Mariana Lopez Correa <mlopecor@emeal.nttdata.com>
2023-05-28 23:54:44 -0500

    Se modifica anotador para tipo de fecha

--- product-service/src/main/java/com/kenox/drugstore/productservice/entity/Medicament.java
+++ product-service/src/main/java/com/kenox/drugstore/productservice/entity/Medicament.java
@@ -4,6 +4,8 @@ package com.kenox.drugstore.productservice.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,9 +32,11 @@ public class Medicament implements Serializable{
     private String name;
 
     @NotNull
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date manufacturingDate;
 
     @NotNull
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date expirationDate;
 
     @NotNull
